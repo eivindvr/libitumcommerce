ac7f161413a246425c28907c0860056e91b8d381
eivind <hello@eivind.se>
2020-02-11 21:18:38 +0100

    removed filters and nav

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,30 +1,10 @@
 @extends('layouts.app')
 
 @section('categories-menu')
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="categoriesMenu">
-            <ul class="navbar-nav">
-            @foreach($taxonomies as $taxonomy)
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ $taxonomy->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        @include('product.index._category_level', ['taxons' => $taxonomy->rootLevelTaxons()])
-                    </div>
-                </li>
-            @endforeach
-            </ul>
-        </div>
-    </nav>
+
 @stop
 
 @section('breadcrumbs')
-    <li class="breadcrumb-item"><a href="{{ route('product.index') }}">All Products</a></li>
     @if($taxon)
         @include('product._breadcrumbs')
     @endif
@@ -33,12 +13,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-            <div class="col-md-3">
-                @include('product.index._filters', ['properties' => $properties, 'filters' => $filters])
-            </div>
-
-            <div class="col-md-9">
+            <div class="col-md-12">
                 @if($taxon && $products->isEmpty() && $taxon->children->count())
                     <div class="card card-default mb-4">
                         <div class="card-header">{{ $taxon->name }} Subcategories</div>
